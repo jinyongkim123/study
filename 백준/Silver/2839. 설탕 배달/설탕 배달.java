66d80e4ef7ec55401476b91965c359f3358fcb33
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-02-25 19:15:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
- 
+import java.util.*;
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-        
-		int N = in.nextInt();
-		
-		
-		if (N == 4 || N == 7) {
-			System.out.println(-1);
-		}
-		else if (N % 5 == 0) {
-			System.out.println(N / 5);
-		}
-		else if (N % 5 == 1 || N % 5 == 3) {
-			System.out.println((N / 5) + 1);
-		}
-		else if (N % 5 == 2 || N % 5 == 4) {
-			System.out.println((N / 5) + 2);
-		}
-	}
+
+    public static void main(String[] args) throws Exception {
+        Scanner stdIn = new Scanner(System.in);
+        int N = stdIn.nextInt();
+
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        } else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        } else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        } else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+    }
 }
+
+
+
+
