013678ebf3d24c7740c53c3229a0db31cbd42d98
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-03-04 21:16:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner stdIn = new Scanner(System.in);
+        long A = stdIn.nextLong();
+        long B = stdIn.nextLong();
+        long C = stdIn.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
+
+
+
+
