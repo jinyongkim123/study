9c11b76f8c7a17c4c97c3dec521b49815ea05f14
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-03-17 15:01:16 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine()+"\n1");
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
+
+
+
+
