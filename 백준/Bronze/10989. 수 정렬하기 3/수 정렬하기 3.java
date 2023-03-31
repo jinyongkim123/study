4408efef49c0c82e0eb5680ac4c419506af57a99
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-03-31 16:49:34 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1664 ms, Memory: 338120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,28 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main{
 
     public static void main(String[] args) throws IOException {
         //Scanner stdIn = new Scanner(System.in);
+
+        int[] cnt = new int[10001];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int[] ary = new int[n];
 
         for(int i=0; i<n; i++){
-            ary[i] = Integer.parseInt(br.readLine());
+            //해당 요소(인덱스) 값 1 증가
+            cnt[Integer.parseInt(br.readLine())]++;
         }
 
-        Arrays.sort(ary);
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<n; i++){
-            sb.append(ary[i]).append('\n');
+
+        for(int i=1; i<10001; i++){
+            //i 값이 개수가 0이 될 때 까지 출력(빈도 수 의미)
+            while(cnt[i]>0){
+                sb.append(i).append('\n');
+                cnt[i]--;
+            }
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
