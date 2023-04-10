3064b0385e60d573f2599bef686df6f23e6f0aff
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-04-10 20:42:07 +0900

    [Silver V] Title: 나이순 정렬, Time: 1288 ms, Memory: 140600 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,50 +1,47 @@
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
+    
+    //Member 클래스를 정적 내부 클래스로 작성
+    private static class Member{
+        int age;
+        String name;
+        
+        Member(int age, String name){
+            this.age = age;
+            this.name = name;
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
 
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //int n = Integer.parseInt(br.readLine());
-
-
-        Scanner stdIn = new Scanner(System.in);
         
-        int n = stdIn.nextInt();
-        //회원 정보를 저장할 Member 클래스 생성
-        class Member {
-            int age;
-            String name;
-
-            Member(int age, String name) {
-                this.age = age;
-                this.name = name;
-            }
-        }
-
-        //회원정보 저장할 ArrayList 만들기
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        
+        //회원 정보를 저장할 ArrayList 생성
         ArrayList<Member> list = new ArrayList<>();
-
-        //회원 정보를 저장할 ArrayList 만들기
-        for (int i = 0; i < n; i++) {
-            int age = stdIn.nextInt();
-            String name = stdIn.next();
+        
+        for(int i=0; i<n; i++){
+            int age = sc.nextInt();
+            String name = sc.next();
             list.add(new Member(age, name));
         }
-
-        //나이순 오름차순 정렬
-        Collections.sort(list, new Comparator<Member>() {
-            @Override
-            public int compare(Member o1, Member o2) {
-                return o1.age - o2.age;
-            }
-        });
-
-        for (Member member : list) {
-            System.out.println(member.age + " " + member.name);
+        
+        //Comparator를 람다식으로 작성
+        Collections.sort(list, ((o1, o2) -> o1.age-o2.age));
+        
+        StringBuilder sb = new StringBuilder();
+        for(Member member : list){
+            sb.append(member.age).append(" ").append(member.name).append("\n");
         }
+
+        System.out.print(sb.toString());
+        
     }
 }
