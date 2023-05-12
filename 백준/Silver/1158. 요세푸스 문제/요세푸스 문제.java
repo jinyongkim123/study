6a56451b8032ba55fc757e71a991d41a959b6e91
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-05-12 15:42:02 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 580 ms, Memory: 294780 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         Queue<Integer> queue = new LinkedList<>();
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         for(int i=1; i<=n; i++){
             queue.add(i);
