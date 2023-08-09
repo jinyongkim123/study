abadde2f31b260f1137ac12f09b66a435d35dcbe
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-08-09 13:20:20 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        StringBuilder sb = new StringBuilder();
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int[] ary = new int[1000001];
+        boolean[] prime = new boolean[b+1];
+        for(int i = 2; i<=b; i++){
+            if(prime[i]) continue;
+            
+            if(i>=a) sb.append(i).append('\n');
 
-        ary[1] = 1;
-        for(int i = 2; i<b; i++){
-            for(int j = 2; i*j <=b; j++){
-                ary[i*j] = 1;
+            for(int j = i + i; j<=b; j+=i){
+                prime[j] = true;
             }
         }
 
-        for(int i=a; i<=b; i++){
-            if(ary[i] == 0){
-                System.out.println(i);
-            }
-        }
+        System.out.println(sb);
 
     }
 
