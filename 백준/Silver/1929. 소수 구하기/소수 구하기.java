42f0a12cc814ffd3c21119302c2057e9ffb8ed6d
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-08-09 13:15:42 +0900

    [Silver III] Title: 소수 구하기, Time: 976 ms, Memory: 37480 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,42 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    // 개선된 소수 판별 함수
-    static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
-        }
-        if (num <= 3) {
-            return true;
-        }
-        if (num % 2 == 0 || num % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= num; i += 6) {
-            if (num % i == 0 || num % (i + 2) == 0) {
-                return false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] ary = new int[1000001];
+
+        ary[1] = 1;
+        for(int i = 2; i<b; i++){
+            for(int j = 2; i*j <=b; j++){
+                ary[i*j] = 1;
             }
         }
-        return true;
-    }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt(); // 시작 숫자
-        int n = scanner.nextInt(); // 끝 숫자
-
-        if (m <= 2) {
-            System.out.println(2);
-            m = 3;
-        }
-        if (m % 2 == 0) {
-            m++;
-        }
-
-        for (int i = m; i <= n; i += 2) {
-            if (isPrime(i)) {
+        for(int i=a; i<=b; i++){
+            if(ary[i] == 0){
                 System.out.println(i);
             }
         }
+
     }
+
 }
