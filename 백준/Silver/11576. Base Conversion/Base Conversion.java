ac782ec96e31e302e3319708d6d203ca9db222c6
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-09-01 16:36:47 +0900

    [Silver V] Title: Base Conversion, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/11576. Base Conversion/Base Conversion.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static boolean[] isComposite;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        int A = Integer.parseInt(st.nextToken()); // 미래사용
+        int B = Integer.parseInt(st.nextToken()); // 정이가 사용
+        int m = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        //10진수로 변환
+        int decimalNum = 0;
+        for(int i=m; i>0; i--){
+            int num = Integer.parseInt(st.nextToken());
+            decimalNum += num * Math.pow(A, i - 1);
+        }
+
+        if(decimalNum == 0){
+            sb.append(0);
+        }
+        //B진수로 변환
+        while(decimalNum != 0){
+            list.add(decimalNum % B);
+            decimalNum/=B;
+        }
+        for(int i = list.size()-1; i>=0; i--){
+            sb.append(list.get(i) + " ");
+        }
+        System.out.println(sb);
+
+    }
+}
