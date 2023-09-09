a705d88979f122e997d8c04a114df8d5c2830415
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-09-09 19:40:24 +0900

    [Silver III] Title: 1로 만들기, Time: 240 ms, Memory: 21652 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,39 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Integer[] dp;
+    static int[] dp;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp = new int[n+1];
 
-        int N = sc.nextInt();
+        dp[1] = 0;
 
-        dp = new Integer[N + 1];
-        dp[0] = dp[1] = 0;
-
-        System.out.print(recur(N));
-
-    }
-
-    static int recur(int N) {
-        if (dp[N] == null) {
-            //6으로 나눠진다면?
-            if (N % 6 == 0) {
-                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+        for(int i = 2; i < n + 1; i++){
+            dp[i] = dp[i-1] + 1;
+            if(i % 2 == 0 && dp[i] > dp[i/2] + 1){
+                dp[i] = dp[i/2] + 1;
             }
-            //3으로 나눠진다면?
-            else if (N % 3 == 0) {
-                dp[N] = Math.min(recur(N/3),recur(N-1))+1;
-            }
-            //2로 나눠진다면?
-            else if (N % 2 == 0) {
-                dp[N] = Math.min( recur(N/2),recur(N-1))+1;
-            }
-            //둘다 안 나눠진다면?
-            else{
-                dp[N] = recur(N - 1) + 1;
+            if( i % 3 == 0 && dp[i] > dp[i/3] + 1){
+                dp[i] = dp[i/3] + 1;
             }
         }
-        return dp[N];
-    }
+        System.out.println(dp[n]);
+    } 
+
 }
