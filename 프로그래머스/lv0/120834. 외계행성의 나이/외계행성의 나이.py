dc54be78ee0348185d732f1fabef3cac91ca0c45
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-09-09 20:11:42 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -2,9 +2,8 @@ def solution(age):
     alpht = "abcdefghij"
     answer = ""
     
-    age_str = str(age)
     
-    for digit in age_str:
+    for digit in str(age):
         answer += alpht[int(digit)]
     
     return answer
