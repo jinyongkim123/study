3d44f0cfcfc289847a7310c1a9b59ff2555ac51a
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-10-02 08:10:45 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 236 ms, Memory: 17924 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner stdIn = new Scanner(System.in);
+
+        while(stdIn.hasNextLine()){
+            String s = stdIn.nextLine();
+
+            System.out.println(s);
+        }
+
+
+    }
+}
+
+
+
+
