c45d4dd63c9eb648fc87a19dabbccb9d2c2d5fb9
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-10-03 20:37:07 +0900

    [Silver III] Title: 1로 만들기, Time: 332 ms, Memory: 78400 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
-
 public class Main {
-
-    static int[] dp;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        dp = new int[n+1];
-
-        dp[1] = 0;
-
-        for(int i = 2; i < n + 1; i++){
-            dp[i] = dp[i-1] + 1;
-            if(i % 2 == 0 && dp[i] > dp[i/2] + 1){
-                dp[i] = dp[i/2] + 1;
-            }
-            if( i % 3 == 0 && dp[i] > dp[i/3] + 1){
-                dp[i] = dp[i/3] + 1;
-            }
-        }
-        System.out.println(dp[n]);
-    } 
-
+ 
+	static Integer[] dp;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+ 
+		dp = new Integer[N + 1];
+		dp[0] = dp[1] = 0;
+ 
+		System.out.print(recur(N));
+ 
+	}
+ 
+	static int recur(int N) {
+ 
+		if (dp[N] == null) {
+			// 6으로 나눠지는 경우 
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			// 3으로만 나눠지는 경우 
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			// 2로만 나눠지는 경우 
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			// 2와 3으로 나누어지지 않는 경우
+			else {
+				dp[N] = recur(N - 1) + 1;
+			}
+		}
+		return dp[N];
+	}
+ 
 }
