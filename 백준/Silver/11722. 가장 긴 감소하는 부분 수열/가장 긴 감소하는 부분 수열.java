5f46a5d44d65ddda0ee99757b473eecb2d69dba8
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-11-06 05:42:48 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 284 ms, Memory: 20884 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N];
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j] && dp[i] <= dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (dp[i] > res) {
+                res = dp[i];
+            }
+        }
+
+        System.out.println(res);
+    }
+}
