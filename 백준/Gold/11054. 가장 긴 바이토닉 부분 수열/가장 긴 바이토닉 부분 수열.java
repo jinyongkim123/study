60d00f76d85280cfe734199ae5b4e6710c72aed2
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-11-07 18:12:05 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 304 ms, Memory: 21092 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp1 = new int[N];
+        int[] dp2 = new int[N];
+        int[] ldp = new int[N];
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            dp1[i] = 1;
+            dp2[i] = 1;
+        }
+        //왼쪽에서부터 i까지 증가하는 수열
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp1[i] <= dp1[j] + 1) {
+                    dp1[i] = dp1[j] + 1;
+                }
+            }
+        }
+        //오른쪽으로부터  i까지 증가하는 수열
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = N-1 ; j > i; j--) {
+                if (arr[i] > arr[j] && dp2[i] <= dp2[j] + 1) {
+                    dp2[i] = dp2[j] + 1;
+                }
+            }
+        }
+
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            result = Math.max(dp1[i]+dp2[i]-1, result);
+        }
+        System.out.println(result);
+    }
+}
