f9ad38fd4a03f0342bd66f361026b466aa41ca99
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-11-10 17:16:03 +0900

    [Silver II] Title: 제곱수의 합, Time: 256 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -4,19 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
+        int N = sc.nextInt();
         int[] dp = new int[100001];
 
-        for(int i = 0; i<=n; i++){
+        for (int i = 0; i <= N; i++) {
             dp[i] = i;
         }
 
-        for(int i = 1; i<= n; i++){
-            for(int j = 1; j*j <= i; j++){
-                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
-        System.out.println(dp[n]);
+        System.out.println(dp[N]);
     }
 }
+
