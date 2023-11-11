99a124262cff699b76825b7a15be63f3d4b06089
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2023-11-12 06:49:08 +0900

    [Gold V] Title: 합분해, Time: 208 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -4,26 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+        int[][] dp = new int[K+1][N+1];
 
-        int[][] dp = new int[k+1][n+1];
-
-        for(int i = 0; i<n+1; i++){
-            dp[1][i] = 1;
-        }
-        for(int i = 0; i<k+1; i++){
+        for(int i =1; i<=K; i++){
             dp[i][0] = 1;
         }
+        for(int i =0; i<=N; i++){
+            dp[1][i] = 1;
+        }
 
-        for(int i = 2; i<=k; i++){
-            for(int j = 1; j<=n; j++){
+
+        for(int i =2 ; i<=K; i++){
+            for(int j = 1; j<=N; j++){
                 dp[i][j] = dp[i-1][j]+dp[i][j-1];
                 dp[i][j] %= 1000000000;
             }
         }
 
-        System.out.println(dp[k][n]);
+        System.out.println(dp[K][N]);
     }
 }
+
