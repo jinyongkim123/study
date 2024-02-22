d4907609489896919164f0f4d1a267debbfc441e
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-22 12:14:30 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,41 @@
+def solution(genres, plays):
+    genrestotal = {}
+    genresnumbering = []
+    
+    
+    for i in range(len(genres)):
+        if genres[i] in genrestotal:
+            genrestotal[genres[i]] += plays[i]
+        else:
+            genrestotal[genres[i]] = plays[i]
+    
+    for i in range(len(genres)):
+        genresnumbering.append([i, genres[i], plays[i]])
+    
+    genrestotal = sorted(genrestotal, key = genrestotal.get, reverse=True)
+    genresnumbering = sorted(genresnumbering, key = lambda x : -x[2])
+    
+    answer = []
+    
+    for i in genrestotal:
+        cnt = 0
+        
+        for j in range(len(genresnumbering)):
+            if i == genresnumbering[j][1]:
+                cnt += 1
+                if cnt == 3:
+                    break
+                answer.append(genresnumbering[j][0])
+                
+            
+#     print(genrestotal)
+    
+    
+#     print(genresnumbering)
+    
+    print(answer)
+    
+    return answer
+    
+
+    
