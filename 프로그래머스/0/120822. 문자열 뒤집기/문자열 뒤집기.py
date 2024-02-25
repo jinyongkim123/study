4a6d9dff31dceae8833f2cbc48b54170c70abd8f
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-25 12:38:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -5,3 +5,9 @@ def solution(my_string):
         reverse_answer.append(answer.pop())
     
     return ''.join(reverse_answer)
+
+def solution(my_string):
+    return my_string[::-1]
+
+def solution(my_string):
+    return ''.join(list(reversed(my_string)))
