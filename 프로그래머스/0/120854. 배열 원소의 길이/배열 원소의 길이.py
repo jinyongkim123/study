d64b70a93159f24b93a3b7b4c10bf388ab6715fe
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-25 12:18:52 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -7,8 +7,11 @@
 #         answer.append(cnt)
 #     return answer
 
+# def solution(strlist):
+#     answer =[]
+#     for i in strlist:
+#         answer.append(len(i))
+#     return answer
+
 def solution(strlist):
-    answer =[]
-    for i in strlist:
-        answer.append(len(i))
-    return answer
+    return [len(str) for str in strlist]
