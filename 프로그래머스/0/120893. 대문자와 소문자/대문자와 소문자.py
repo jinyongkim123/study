b895705a4def0938c87700a3a08c9e547736bb10
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-26 20:51:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.59 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,12 +1,17 @@
 def solution(my_string):
     answer = ''
-    print(ord('a'))
-    print(ord('A'))
-    
     for i in my_string:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             answer += chr(ord(i) - 32)
         if ord(i) >= ord('A') and ord(i) <= ord('Z'):            
             answer += chr(ord(i) + 32)
-
     return answer
+
+    answer = ''
+    for i in my_string:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            answer += i.upper()
+        if ord(i) >= ord('A') and ord(i) <= ord('Z'):            
+            answer += i.lower()
+    return answer
+
