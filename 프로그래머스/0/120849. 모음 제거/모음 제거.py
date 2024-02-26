e7736602418786aa7ec8d854f3f74bbabb3abba7
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-26 19:39:35 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,7 @@
 def solution(my_string):
-    my_string = list(my_string)
     answer = ''
-    for i in range(len(my_string)):
-        if my_string[i] != 'a' and my_string[i] != 'e' and my_string[i] != 'i' and my_string[i] != 'o' and my_string[i] != 'u': answer += my_string[i]
+    for i in my_string:
+        if i not in 'aeiou':
+            answer += i
     return answer
+        
