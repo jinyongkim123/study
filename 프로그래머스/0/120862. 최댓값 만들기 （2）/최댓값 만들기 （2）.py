106ae423d78de1ef30a632dd58d4c5ba277f89ee
jinyongkim123 <117449640+jinyongkim123@users.noreply.github.com>
2024-02-26 21:10:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,10 +1,4 @@
 from itertools import combinations
 def solution(numbers):
-    answer = []
-    sol = []
-    answer = list(combinations(numbers, 2))
-    
-    for i in answer:
-        sol.append(i[0]*i[1])
-    
-    return max(sol)
+    numbers.sort()
+    return max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
